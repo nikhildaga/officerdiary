f2153894206b560ee6b641eaa2272118f051d1f5
Nikhil Daga <nikhil.daga.bitsian@gmail.com>
2015-06-26 04:33:53 +0530

    set min height to 100%

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,61 +1,80 @@
 Template.layout.rendered = function() {
-	$('.sidebar-toggle').each(function() {
-        var group = $(this);
-        $(this).find(".btn").click(function(e) {
-            group.find(".btn.active").removeClass("active");
-            $(this).addClass("active");
-            e.preventDefault();
-        });
-    });
+    //Get window height and the wrapper height
+    var neg = $('.main-header').outerHeight() + $('.main-footer').outerHeight();
+    var window_height = $(window).height();
+    var sidebar_height = $(".sidebar").height();
+    //Set the min-height of the content and sidebar based on the
+    //the height of the document.
+    if ($("body").hasClass("fixed")) {
+      $(".content-wrapper, .right-side").css('min-height', window_height - $('.main-footer').outerHeight());
+    } else {
+      var postSetWidth;
+      if (window_height >= sidebar_height) {
+        $(".content-wrapper, .right-side").css('min-height', window_height - neg);
+        postSetWidth = window_height - neg;
+      } else {
+        $(".content-wrapper, .right-side").css('min-height', sidebar_height);
+        postSetWidth = sidebar_height;
+      }
 
-    $('.sidebar-toggle').click(function(e) {
-        e.preventDefault();
-        //Enable sidebar push menu
-        $("body").toggleClass('sidebar-collapse');
-        $("body").toggleClass('sidebar-open');
+    }
+  $('.sidebar-toggle').each(function() {
+    var group = $(this);
+    $(this).find(".btn").click(function(e) {
+      group.find(".btn.active").removeClass("active");
+      $(this).addClass("active");
+      e.preventDefault();
     });
-    $(".content-wrapper").click(function() {
-        //Enable hide menu when clicking on the content-wrapper on small screens
-        if ($(window).width() <= 767 && $("body").hasClass("sidebar-open")) {
-            $("body").removeClass('sidebar-open');
-        }
-    });
+  });
 
-    $("li a", $('.sidebar')).click(function(e) {
-        //Get the clicked link and the next element
-        var $this = $(this);
-        var checkElement = $this.next();
+  $('.sidebar-toggle').click(function(e) {
+    e.preventDefault();
+    //Enable sidebar push menu
+    $("body").toggleClass('sidebar-collapse');
+    $("body").toggleClass('sidebar-open');
+  });
+  $(".content-wrapper").click(function() {
+    //Enable hide menu when clicking on the content-wrapper on small screens
+    if ($(window).width() <= 767 && $("body").hasClass("sidebar-open")) {
+      $("body").removeClass('sidebar-open');
+    }
+  });
 
-        //Check if the next element is a menu and is visible
-        if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible'))) {
-            //Close the menu
-            checkElement.slideUp('normal', function() {
-                checkElement.removeClass('menu-open');
-            });
-            checkElement.parent("li").removeClass("active");
-        }
-        //If the menu is not visible
-        else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
-            //Get the parent menu
-            var parent = $this.parents('ul').first();
-            //Close all open menus within the parent
-            var ul = parent.find('ul:visible').slideUp('normal');
-            //Remove the menu-open class from the parent
-            ul.removeClass('menu-open');
-            //Get the parent li
-            var parent_li = $this.parent("li");
+  $("li a", $('.sidebar')).click(function(e) {
+    //Get the clicked link and the next element
+    var $this = $(this);
+    var checkElement = $this.next();
 
-            //Open the target menu and add the menu-open class
-            checkElement.slideDown('normal', function() {
-                //Add the class active to the parent li
-                checkElement.addClass('menu-open');
-                parent.find('li.active').removeClass('active');
-                parent_li.addClass('active');
-            });
-        }
-        //if this isn't a link, prevent the page from being redirected
-        if (checkElement.is('.treeview-menu')) {
-            e.preventDefault();
-        }
-    });
+    //Check if the next element is a menu and is visible
+    if ((checkElement.is('.treeview-menu')) && (checkElement.is(':visible'))) {
+      //Close the menu
+      checkElement.slideUp('normal', function() {
+        checkElement.removeClass('menu-open');
+      });
+      checkElement.parent("li").removeClass("active");
+    }
+    //If the menu is not visible
+    else if ((checkElement.is('.treeview-menu')) && (!checkElement.is(':visible'))) {
+      //Get the parent menu
+      var parent = $this.parents('ul').first();
+      //Close all open menus within the parent
+      var ul = parent.find('ul:visible').slideUp('normal');
+      //Remove the menu-open class from the parent
+      ul.removeClass('menu-open');
+      //Get the parent li
+      var parent_li = $this.parent("li");
+
+      //Open the target menu and add the menu-open class
+      checkElement.slideDown('normal', function() {
+        //Add the class active to the parent li
+        checkElement.addClass('menu-open');
+        parent.find('li.active').removeClass('active');
+        parent_li.addClass('active');
+      });
+    }
+    //if this isn't a link, prevent the page from being redirected
+    if (checkElement.is('.treeview-menu')) {
+      e.preventDefault();
+    }
+  });
 }
