db7d2915418f66ba218c11751f47471a5a2ca067
Nikhil Daga <nikhil.daga.bitsian@gmail.com>
2015-06-26 03:48:29 +0530

    people collection

--- people/both/collections/people.js
+++ people/both/collections/people.js
@@ -1,7 +1,46 @@
 People = new Mongo.Collection("People");
 
 People.attachSchema(new SimpleSchema({
-  name: {
+  firstName: {
+    type: String
+  },
+  lastName: {
+    type: String
+  },
+  mobile: {
+    type: Number
+  },
+  email: {
+    type: String,
+    regEx: SimpleSchema.RegEx.Email
+  },
+  jobTitle: {
+    type: String
+  },
+  company: {
+    type: String
+  },
+  location: {
+    type: String,
+    autoform: {
+      type: 'map',
+      afFieldInput: {
+        searchBox: true
+      }
+    }
+  },
+  positionData: {
+    type: String,
+    autoform: {
+      type: 'map',
+      afFieldInput: {
+        searchBox: true,
+        geolocation: true,
+        autolocate: true
+      }
+    }
+  },
+  feedData: {
     type: String
   }
 }));
